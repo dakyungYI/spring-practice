4fc0ff2f96f847adcaf9163a96e750946391442b
dakyung <leeda407@gmail.com>
2023-06-09 15:32:05 +0900

    레거시 코드 개선 후

--- section3-week1/src/main/java/com/codestates/member/MemberController.java
+++ section3-week1/src/main/java/com/codestates/member/MemberController.java
@@ -1,45 +1,52 @@
 package com.codestates.member;
 
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping(value = "/v1/members", produces = {MediaType.APPLICATION_JSON_VALUE})
+@RequestMapping(value = "/v1/members")
 public class MemberController {
 /*
 요청에 필요한 회원(Member) 정보
 1. 회원 이메일 주소: email
 2. 회원 이름: name
 3. 회원 전화번호: phoneNumber
+memberId(회원 식별자): long
+email(이메일): String
+name(이름): String
+phone(휴대폰 번호): String 예) 010-1234-5678
  */
     @PostMapping
-    public String postMember(@RequestParam("email") String email,
-                             @RequestParam("name") String name,
-                             @RequestParam("phone") String phone) {
+    public ResponseEntity postMember(@RequestParam("email") String email,
+                                     @RequestParam("name") String name,
+                                     @RequestParam("phone") String phone) {
         System.out.println("# email: " + email);
         System.out.println("# name: " + name);
         System.out.println("# phone: " + phone);
 
-        String response =
-                "{\"" +
-                    "email\":\""+email+"\"," +
-                    "\"name\":\""+name+"\",\"" +
-                    "phone\":\"" + phone+
-                "\"}";
-        return response;
+        Map<String, String> map = new HashMap<>();
+        map.put("email", email);
+        map.put("name", name);
+        map.put("phone", phone);
+
+        return new ResponseEntity<>(map, HttpStatus.CREATED);
     }
 
     @GetMapping("/{member-id}")
-    public String getMember(@PathVariable("member-id") long memberId) {
+    public ResponseEntity getMember(@PathVariable("member-id") long memberId) {
         System.out.println("# memberId: " + memberId);
 
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
-    public String getMembers() {
+    public ResponseEntity getMembers() {
         System.out.println("# get Members");
 
-        return null;
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
